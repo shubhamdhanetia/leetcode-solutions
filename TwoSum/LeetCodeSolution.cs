8888893744fc48582f0d2838958421fd6a0423d8
Shubham Dhanetia <shubham.dhanetia@gmail.com>
2021-05-02 11:31:48 +0530

    Two Sum

---
+++ TwoSum/LeetCodeSolution.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TwoSum
+{
+    public class LeetCodeSolution
+    {
+        public int[] twoSumSorted(int[] numbers, int target) {
+
+            int a_pointer = 0;
+            int b_pointer = numbers.Length - 1;
+
+            while (a_pointer <= b_pointer) {
+
+                int sum = numbers[a_pointer] + numbers[b_pointer];
+
+                if (sum > target)
+                {
+                    b_pointer--;
+                }
+                else if (sum < target)
+                {
+                    a_pointer++;
+                }
+                else {
+                    return new int[] {a_pointer+1 , b_pointer+1 };
+                }
+
+            }
+
+            return new int[] { a_pointer + 1, b_pointer + 1 };
+        }
+    }
+}
---
+++ TwoSum/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace TwoSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LeetCodeSolution solution = new LeetCodeSolution();
+            int[] array = new int[6] { 1, 2, 3, 4, 23, 25};
+            var indices = solution.twoSumSorted(array, 26);
+            Console.WriteLine(indices);
+        }
+    }
+}
