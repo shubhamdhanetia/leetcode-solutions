a7655461bd2a069febed7592144fbe3f6b2bff7b
Shubham Dhanetia <shubham.dhanetia@gmail.com>
2021-05-04 15:05:47 +0530

    NaryTree and BinaryTree

---
+++ BinaryTree/BTree.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BinaryTree
+{
+    public class BTree
+    {
+        private Node _root;
+
+        public BTree() {
+            _root = null;
+        }
+
+        public void Insert(int data) {
+
+            if (_root == null) {
+                _root = new Node(data);
+                 return;
+            }
+
+            InsertRec(_root,new Node(data));
+        }
+
+        private void InsertRec(Node root, Node newNode) {
+
+            if (root == null) {
+                root = newNode;
+            }
+
+            if (newNode.Data < root.Data)
+            {
+
+                if (root.Left == null)
+                {
+                    root.Left = newNode;
+                }
+                else
+                {
+                    InsertRec(root.Left, newNode);
+                }
+            }
+            else {
+                if (root.Right == null) {
+                    root.Right = newNode;
+                }
+                else{
+                    InsertRec(root.Right, newNode);
+                }
+            }
+
+        }
+
+        private void DisplayTree(Node root) {
+
+            if (root == null) return;
+
+            DisplayTree(root.Left);
+            Console.Write(root.Data + " ");
+            DisplayTree(root.Right);
+
+        }
+
+        public void DisplayTree() {
+
+            DisplayTree(_root);
+        }
+
+        public IEnumerable<int> InOrderTraversal() {
+
+            Stack<Node> stack = new Stack<Node>();
+            List<int> output = new List<int>();
+            if (_root == null) {
+                return output;
+            }
+
+            Node currentNode = _root;
+
+            while (currentNode != null || stack.Count != 0) {
+
+                while (currentNode != null) {
+                    stack.Push(currentNode);
+                    currentNode = currentNode.Left;
+                }
+
+                currentNode = stack.Pop();
+                output.Add(currentNode.Data);
+                currentNode = currentNode.Right;
+            }
+
+            return output;
+        }
+
+    }
+
+    public class Node { 
+    
+        public int Data { get; set; }
+        public Node Left { get; set; }
+        public Node Right { get; set; }
+        public Node() {         
+        }
+        public Node(int data) {
+            this.Data = data;
+        }
+
+    
+    }
+}
---
+++ BinaryTree/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace BinaryTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BTree bTree = new BTree();
+            bTree.Insert(10);
+            bTree.Insert(8);
+            bTree.Insert(12);
+            bTree.Insert(11);
+            bTree.Insert(7);
+            bTree.Insert(9);
+            var result = bTree.InOrderTraversal();
+
+
+        }
+    }
+} 
